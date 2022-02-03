b49a6692a11c94a6d24f85e65b87395b0e8624ec
Newdevcreator <newdevcreator@gmail.com>
2022-02-03 14:00:41 +0100

    init

---
+++ controllers/MainController.php
@@ -0,0 +1,99 @@
+<?php
+/************* MainController ******************* 
+  
+Je suis le seul controller (pour l'instant)
+
+Mon rôle est de donner les informations nécéssaires
+à l'affichage d'une vue.
+
+C'est aussi moi qui fait les require
+je suis donc en charge de la méthode show()
+
+*******************************************/
+
+class MainController {
+
+    private $weekOpeningHours;
+
+    /**
+     * Construteur où je met mes valeurs par défaut
+     */
+    public function __construct()
+    {
+        // en prévision d'aller les chercher en BDD (fin de saison 5)
+        $this->weekOpeningHours = [
+            'Sunday' => 'Closed', 
+            'Monday' => '7:00 AM to 8:00 PM',
+            'Tuesday' => '12:12 AM to 8:00 PM',
+            'Wednesday' => '7:00 AM to 8:00 PM',
+            'Thursday' => '7:00 AM to 8:00 PM',
+            'Friday' => '7:00 AM to 8:00 PM',
+            'Saturday' => '9:00 AM to 5:00 PM'
+        ];
+    }
+
+
+    public function affichePageStore(){
+        // j'utilise la méthode show avec les bons paramètres
+        // je sais que cette méthode doit affiche la page store, obvious, le nom de la méthode
+        $this->show('store', $this->weekOpeningHours);
+    }
+
+
+    public function affichePageHome(){
+        // j'utilise la méthode show avec les bons paramètres
+        // je sais que cette méthode doit affiche la page store, obvious, le nom de la méthode
+        $this->show('home', "Je suis la page HOME");
+    }
+
+    public function affichePageProducts(){
+        // j'utilise la méthode show avec les bons paramètres
+        // je sais que cette méthode doit affiche la page store, obvious, le nom de la méthode
+        $this->show('products', "Je suis la page PRODUCTS");
+    }
+
+    public function affichePageAbout(){
+        // j'utilise la méthode show avec les bons paramètres
+        // je sais que cette méthode doit affiche la page store, obvious, le nom de la méthode
+        $this->show('about', "Je suis la page ABOUT");
+    }
+/**
+ * Fonction qui require les templates HEADER / FOOTER
+ * Ainsi que la vue donnée en paramètre
+ * Et surtout les informations nécessaire/essentielle à l'affichage vue
+ *
+ * @param string $viewName le nom du fichier template à inclure
+ * @param $viewData Contient toutes les informations que j'ai besoin pour l'affichage
+ */
+/* avant de rentrer dans la fonction show
+ $viewName = $currentPage;
+ $viewData = $weekOpeningHours;
+*/
+public function show($viewName, $viewData)
+{
+    require_once __DIR__.'/../views/header.tpl.php';
+
+    /****************************************** */
+    // exemples : require_once __DIR__."/views/{$viewName}.tpl.php";
+    // si $viewName == 'home'   
+    // require_once __DIR__."/views/home.tpl.php";
+    // si $viewName == 'products'   
+    // require_once __DIR__."/views/products.tpl.php";
+    // si $viewName == 'store'   
+    // require_once __DIR__."/views/store.tpl.php";
+    /****************************************** */
+    
+    // ici je peux utiliser $viewData
+    // donc dans le require aussi
+
+    require_once __DIR__."/../views/{$viewName}.tpl.php";
+    require_once __DIR__.'/../views/footer.tpl.php';
+
+    /* A la manière de la Saison 4 */
+    //require __DIR__.'/views/header.tpl.php';
+    // require __DIR__."/views/{$currentPage}.tpl.php";
+    // Autre manière de concatener notre variable
+    // require __DIR__.'/views/'.$currentPage.'.tpl.php';
+    //require __DIR__.'/views/footer.tpl.php';
+}
+}
---
+++ views/store.tpl.php
@@ -0,0 +1,44 @@
+<?php
+//echo "<h1 style=\"color:#FFF\">{$viewData}</h1>";
+?>
+<section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h2 class="section-heading mb-5">
+                <span class="section-heading-upper">Come On In</span>
+                <span class="section-heading-lower">We're Open</span>
+              </h2>
+              <ul class="list-unstyled list-hours mb-5 text-left mx-auto">
+              <?php 
+              // exemple de ligne : 'Monday' => '7:00 AM to 8:00 PM'
+              // $day = 'Monday'
+              // $openingHours = '7:00 AM to 8:00 PM'
+              foreach ($viewData as $day => $openingHours) : 
+              ?>
+              <li class="list-unstyled-item list-hours-item d-flex">
+                  <?= $day ?>
+                  <span class="ml-auto"><?= $openingHours ?></span>
+                </li>
+              <?php endforeach ?>
+              </ul>
+              <p class="address mb-5">
+                <em>
+                  <strong>1116 Orchard Street</strong>
+                  <br>
+                  Golden Valley, Minnesota
+                </em>
+              </p>
+              <p class="mb-0">
+                <small>
+                  <em>Call Anytime</em>
+                </small>
+                <br>
+                (317) 585-8468
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
